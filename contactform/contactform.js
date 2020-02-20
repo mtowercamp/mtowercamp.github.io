902eb6e75a4088d7c9abb6dfe122cd94d02ee37e
mtowercamp <mtowercamp@gmail.com>
2020-02-20 14:18:33 +0800

    Save

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -107,19 +107,19 @@ jQuery(document).ready(function($) {
       }
     });
     if (ferror) return false;
-    else var str = $(this).serialize();
-    var action = $(this).attr("action");
-    if (!action) {
-      action = "contactform/contactform.php";
-    }
-    $.ajax({
-      type: "POST",
-      url: action,
-      data: str,
-      success: function(msg) {
-        window.open(msg);
-      }
-    });
-    return false;
+    // else var str = $(this).serialize();
+    // var action = $(this).attr("action");
+    // if (!action) {
+    //   action = "contactform/contactform.php";
+    // }
+    // $.ajax({
+    //   type: "POST",
+    //   url: action,
+    //   data: str,
+    //   success: function(msg) {
+    //     window.open(msg);
+    //   }
+    // });
+    // return false;
   });
 });
